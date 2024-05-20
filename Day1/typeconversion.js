03c8136ceefd2685326dc253c1bcf73661b32bb7
kripasagar3 <kripasagarsivakumar@gmail.com>
2024-05-20 17:55:55 +0530

    Add files via upload

---
+++ Day1/typeconversion.js
@@ -0,0 +1,28 @@
+let typ=100;
+let typ1="you have";
+console.log(typ1/typ);
+console.log("implicit conversion");
+{
+    let val1="20";
+    let val2=100;
+    console.log(val1*val2);
+}
+{
+    let val1="you have";
+    let val2=200;
+    console.log(val1+val2);
+}
+console.log("Explicit conversion");
+{
+    let val1=Boolean("how many students");
+    
+    let val2=Number("40")
+    let val3=Number("hi how are you");
+    let val4=Boolean("");
+    console.log(val1);
+    console.log(val3);
+    console.log(val1+val2);
+
+    console.log(val4);
+
+}
