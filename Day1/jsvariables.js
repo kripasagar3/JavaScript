2cd408eacb10ea90757a6679b8e7e7c539f5e56e
kripasagar3 <kripasagarsivakumar@gmail.com>
2024-05-20 17:53:54 +0530

    Create jsvariables.js

---
+++ Day1/jsvariables.js
@@ -0,0 +1,33 @@
+'use strict'
+    function disp()
+    {
+            let greet1="bye"
+                console.log(greet1);
+            }
+            disp();
+        
+            //const name;
+        //name="pavi";
+        const name="lakshman"
+        {
+              let name="ram" //it does not alloe to change. 
+                 console.log(name);
+                 //constant doesn't execute when it out side the  
+                }
+                function star()
+                {
+                        var x=5,i=0,j=0;   
+                         for (i=0;i<=x;i++)
+                            {    
+                                for(j=0;j<i;j++) 
+                                       {   
+                                         console.log("*");
+                                       }
+                                       console.log("print in js");
+                                    }
+                                }star();
+//const sample="hi";
+  {
+    const up="hi";
+    console.log(up.toUpperCase());
+  }
