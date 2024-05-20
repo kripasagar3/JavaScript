d963e9f5b142a1c1ce9271559e6ef1177cb3731e
kripasagar3 <kripasagarsivakumar@gmail.com>
2024-05-20 17:55:10 +0530

    Add files via upload

---
+++ Day1/datatypes.js
@@ -0,0 +1,42 @@
+let x=2**53;
+   
+function max()
+{
+   
+    console.log(`the maxi int${x-1}`);
+    
+}max();
+
+let val=true;
+
+ console.log(typeof(val));
+
+ let sample ="the value is";
+
+ console.log(sample/x);
+ 
+ function types()
+ {
+     let val1="hi";
+
+    console.log(typeof(val1));
+
+   let  val2;
+
+    console.log(typeof(val2));
+
+   let val3=50;
+
+    console.log(typeof(val3));
+
+    let val4=true;
+
+    console.log(typeof(val4));
+
+    let val5=null;
+    
+    console.log(typeof(val5));
+
+ }types();
+
+ 
