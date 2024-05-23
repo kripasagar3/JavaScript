c7f53b452e5babc097dcb8a7b0690486b5acdc4f
kripasagar3 <kripasagarsivakumar@gmail.com>
2024-05-23 15:45:34 +0530

    Add files via upload

---
+++ Day4/object3.js
@@ -0,0 +1,11 @@
+const teacher={
+    tname:"suresh",
+    age:33,
+    place:"coimbatroe",
+    subject:"maths",
+};
+Object.defineProperty(teacher,"qualification",{value:"B.ed",writable:false});
+Object.seal(teacher);
+console.log(teacher);
+delete teacher.age;
+console.log(teacher);
