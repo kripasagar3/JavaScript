70d09ce1546cda6698438af9404794161e407a9f
kripasagar3 <kripasagarsivakumar@gmail.com>
2024-05-24 17:49:45 +0530

    Add files via upload

---
+++ Day5/symbols.js
@@ -0,0 +1,21 @@
+// let student={
+//     name:"harish",
+//     age:22,
+//     place:"tirupur"
+// }
+// console.log(Object.getOwnPropertyNames(student));
+/////////
+
+let tname=Symbol ('symb1');
+let age=Symbol('symb2')
+let teacher={
+    [tname]:"prasath",
+    place:"salem",
+    [age]:23,
+}
+console.log(Object.getOwnPropertySymbols(teacher));
+console.log(teacher);
+console.log(teacher[tname]="harish");
+console.log(teacher);
+console.log(delete teacher[tname]);
+console.log(teacher);
