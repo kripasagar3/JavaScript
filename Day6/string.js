d54451f132aadd28ae5428ff1b48167aeb4d5c0f
kripasagar3 <kripasagarsivakumar@gmail.com>
2024-05-27 16:44:57 +0530

    Add files via upload

---
+++ Day6/string.js
@@ -0,0 +1,34 @@
+const str="hi@ this is ram from salem \t yercad is located near to salem \n salem is famous for mangos ";
+
+const str1="welcome to java script programming language";
+
+const str2="This is kripasagar";
+
+console.log(str2.replace("kripasagar","prabash"));
+let len=(str.length);
+console.log(len);
+console.log(str1.charAt(3));
+console.log(str1.slice(2));
+console.log(str.charCodeAt(2));
+console.log(str.split(" "));
+console.log(str1.concat(str2));
+console.log(str.constructor);
+console.log(str1.endsWith("program"));
+//console.log(str.fromCharCode(65,23,32,107));
+console.log(str.includes("ram"));
+console.log(str2.indexOf("is"));
+console.log(str1.lastIndexOf("language"));
+console.log(str1.localeCompare(str2));
+console.log(str.match("m/is"));
+console.log(str2.padEnd(3,"..."));
+console.log(str1.padStart(1,"hii"));
+console.log(str2.normalize("NFC"));
+console.log(str1.concat(str2));
+console.log(str2.repeat(4));
+console.log(str.toLowerCase);
+console.log(str.substring(2,10));
+console.log(str.split(5));
+
+
+
+
