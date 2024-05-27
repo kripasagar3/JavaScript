ce1b762c0c1f886aa501889cf4cf54daf8a5fe64
kripasagar3 <kripasagarsivakumar@gmail.com>
2024-05-27 16:46:31 +0530

    Add files via upload

---
+++ Day6/map.js
@@ -0,0 +1,36 @@
+let empleoye=["ram ","sam  ","somu  ","ganesh"]
+
+console.log(empleoye.map(x=>x.toUpperCase().trim()));
+
+let student =new Map();
+student.set(1,"prakash");
+console.log(student.get(1));
+
+// function mapUse()
+// {
+//     let teacher=[""]
+// }
+let cost=new Map();
+cost.set("orange",80);
+cost.set("apple",76);
+cost.set("graps",  68);
+cost.set("banaba 1pice",8);
+ console.log(cost.get("graps"))
+ for(i in cost)
+    {
+        return cost.get();
+    }
+    console.log(cost);
+
+    let statu=new Set();
+    statu.add("pencil",15);
+    statu.add("pen",34);
+    statu.add("paper",2);
+    statu.add("sharpner",45);
+    statu.add("eraser",5);
+    statu.add("scale",15);
+    for(j in statu){
+
+   return statu[j];
+    }
+    console.log(statu);
