c15c103ca682740c6b1eb6043c28ba39f35244e6
kripasagar3 <kripasagarsivakumar@gmail.com>
2024-05-27 17:05:46 +0530

    Add files via upload

---
+++ Day5/arraymethod.js
@@ -0,0 +1,11 @@
+var car=new Array("maruthi","tata","benze","BMW","Audi");
+console.log(car);
+
+var car2=[
+    "koenigsegg Agera",
+    "mustang",
+    "ferrari",
+    "lamborgini",
+];
+console.log()
+car2.copyWithin()
